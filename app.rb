33fb1d33ef9263016a7ad1c89c60b93850917cdc
grek97 <omarnabiev20@yandex.ru>
2020-05-23 14:36:44 +0300

    first commit

---
+++ app.rb
@@ -0,0 +1,10 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+#таким образом наша главная страница будет возвращать наше представление index.erb 
+# get - получить данные вкладки My website эта вкладка вернет тело index.erb 
+	erb :index
+end
